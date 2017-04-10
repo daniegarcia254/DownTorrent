4506ef3f2e29e19544d59c263aa7fb2e33095782
daniegarcia254 <daniegarcia254@gmail.com>
2017-04-10 23:01:16 +0200

    Improve start-dev.sh script

--- front/start-dev.sh
+++ front/start-dev.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sed -i 's/BACKEND_PORT_VALUE/10002/g' src/main.js
-sed -i 's/BACKEND_URL_CORDOVA/"http\/\/danigarcia-dev.com"/g' src/main.js
-sed -i 's/BACKEND_PORT_VALUE/10002/g' src/store/store.js
-sed -i 's/BACKEND_URL_CORDOVA/"http\/\/danigarcia-dev.com"/g' src/store/store.js
+BACKEND_PORT=10002
+URL_CORDOVA="http:\/\/danigarcia.dev.com"
+
+sed -i 's/BACKEND_PORT_VALUE/'$BACKEND_PORT'/g' src/main.js
+sed -i 's/BACKEND_URL_CORDOVA/"'$URL_CORDOVA'"/g' src/main.js
+sed -i 's/BACKEND_PORT_VALUE/'$BACKEND_PORT'/g' src/store/store.js
+sed -i 's/BACKEND_URL_CORDOVA/"'$URL_CORDOVA'"/g' src/store/store.js
 
 # Start Quasar dev
 quasar dev --play
